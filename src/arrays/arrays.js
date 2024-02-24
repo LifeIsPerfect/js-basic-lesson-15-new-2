a45512fabdbcdbd8882a84092a138d32d7851ef5
Alexander Skripko <lifeisperfect3@gmail.com>
2024-02-24 11:33:01 +0300

    Apply ESLint to an existing code;

--- src/arrays/arrays.js
+++ src/arrays/arrays.js
@@ -1,30 +1,31 @@
-const sum = (arr) => {
-  return arr.reduce((accumulator, value) => accumulator + value, 0);
+const sum = ( arr ) => {
+  return arr.reduce( ( accumulator, value ) => accumulator + value, 0 );
 };
 
-const doubleArrayValues = (arr) => {
-  return arr.map((el) => 2 * el);
+const doubleArrayValues = ( arr ) => {
+  return arr.map( ( el ) => 2 * el );
 };
 
-const findMaxMin = function (nums) {
-  let result = {
+const findMaxMin = function( nums ) {
+  const result = {
     max: undefined,
     min: undefined,
   };
 
-  if (nums.length == 0) {
+  if( nums.length === 0 ) {
     return result;
   }
 
   result.max = nums[0];
   result.min = nums[0];
 
-  // Можно сделать сортировку и взять элементы по краям. Удобно, но медленее, чем так, как ниже, потому что сложность сортировки в лучше случае O(n*log(n)) а у кода ниже сложность линейная O(n).
-  for (let el of nums) {
-    if (el < result.min) {
+  // Можно сделать сортировку и взять элементы по краям. Это удобно, но медленее, чем так, как ниже,
+  // потому что сложность сортировки в среднем O(n*log(n)) а у кода ниже сложность линейная O(n).
+  for( const el of nums ) {
+    if( el < result.min ) {
       result.min = el;
     }
-    if (el > result.max) {
+    if( el > result.max ) {
       result.max = el;
     }
   }
