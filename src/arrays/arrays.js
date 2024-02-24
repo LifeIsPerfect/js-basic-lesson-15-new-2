973527a7eebb3a6766ffab2cb269384b4a3a72d8
Alexander Skripko <lifeisperfect3@gmail.com>
2024-02-24 10:37:41 +0300

    Apply prettier to an existing code;

--- src/arrays/arrays.js
+++ src/arrays/arrays.js
@@ -1,35 +1,34 @@
-const sum = ( arr ) => {
-    return arr.reduce( (accumulator, value) => accumulator + value, 0 );
-}
+const sum = (arr) => {
+  return arr.reduce((accumulator, value) => accumulator + value, 0);
+};
 
-const doubleArrayValues = ( arr ) => {
-    return arr.map( el => 2*el );
-}
+const doubleArrayValues = (arr) => {
+  return arr.map((el) => 2 * el);
+};
 
-const findMaxMin = function( nums ) {
-    let result = {
-        max : undefined,
-        min : undefined,
-    }
+const findMaxMin = function (nums) {
+  let result = {
+    max: undefined,
+    min: undefined,
+  };
 
-    if( nums.length == 0 ){
-        return result;
-    }
-
-    result.max = nums[0];
-    result.min = nums[0];
-
-    // Можно сделать сортировку и взять элементы по краям. Удобно, но медленее, чем так, как ниже, потому что сложность сортировки в лучше случае O(n*log(n)) а у кода ниже сложность линейная O(n).
-    for( let el of nums )
-    {
-      if( el < result.min ) {
-        result.min = el;
-      }
-      if( el > result.max ) {
-        result.max = el;
-      }
-    }
+  if (nums.length == 0) {
     return result;
- }
+  }
+
+  result.max = nums[0];
+  result.min = nums[0];
+
+  // Можно сделать сортировку и взять элементы по краям. Удобно, но медленее, чем так, как ниже, потому что сложность сортировки в лучше случае O(n*log(n)) а у кода ниже сложность линейная O(n).
+  for (let el of nums) {
+    if (el < result.min) {
+      result.min = el;
+    }
+    if (el > result.max) {
+      result.max = el;
+    }
+  }
+  return result;
+};
 
 module.exports = { sum, doubleArrayValues, findMaxMin };
--- src/arrays/arrays.test.js
+++ src/arrays/arrays.test.js
@@ -1,91 +1,89 @@
 const { sum, doubleArrayValues, findMaxMin } = require('./arrays.js');
 
-describe("Homework #5, task #1", () => {
-    describe("Typical cases", () => {
-        it("Case #1", () => {
-            console.log( sum( [1, 2, 3] ) );
-            expect( sum( [1, 2, 3] ) ).toBe( 6 );
-        })
-    } )
+describe('Homework #5, task #1', () => {
+  describe('Typical cases', () => {
+    it('Case #1', () => {
+      console.log(sum([1, 2, 3]));
+      expect(sum([1, 2, 3])).toBe(6);
+    });
+  });
 
-    describe("Edge cases", () => {
-        it("Case #1. Empty array", () => {
-            expect( sum( [] ) ).toBe( 0 );
-        })
-        
-        it("Case #2. Single element array", () => {
-            expect( sum( [7] ) ).toBe( 7 );
-        })
-    } )
-})
+  describe('Edge cases', () => {
+    it('Case #1. Empty array', () => {
+      expect(sum([])).toBe(0);
+    });
 
-describe("Homework #5, task #2", () => {
-    describe("Checking typical case", () => {
-        expect( doubleArrayValues( [1, 2, 3] ) ).toStrictEqual( [2, 4, 6] );
-    } )
+    it('Case #2. Single element array', () => {
+      expect(sum([7])).toBe(7);
+    });
+  });
+});
 
-    describe("Checking edge cases", () => {
-        it("Empty array 0", () => {
-            expect( sum( [] ) ).toStrictEqual( [] );
-        })
-    } )
-})
+describe('Homework #5, task #2', () => {
+  describe('Checking typical case', () => {
+    expect(doubleArrayValues([1, 2, 3])).toStrictEqual([2, 4, 6]);
+  });
 
+  describe('Checking edge cases', () => {
+    it('Empty array 0', () => {
+      expect(sum([])).toStrictEqual([]);
+    });
+  });
+});
 
 findMaxMin = require('./arrays.js');
 
-describe( "Homework #5, task #3", () => {
-    describe( "Typical cases", () => {
-        it( "Case #1", () => {
-            expect(findMaxMin([1, 2, 3, 4])).toStrictEqual( {
-                max: 4,
-                min: 1,
-            } );
-        })
+describe('Homework #5, task #3', () => {
+  describe('Typical cases', () => {
+    it('Case #1', () => {
+      expect(findMaxMin([1, 2, 3, 4])).toStrictEqual({
+        max: 4,
+        min: 1,
+      });
+    });
 
-        it( "Case #2", () => {
-            expect(findMaxMin([4, 3, 2, 1])).toStrictEqual( {
-                max: 4,
-                min: 1,
-            } );
-        })
+    it('Case #2', () => {
+      expect(findMaxMin([4, 3, 2, 1])).toStrictEqual({
+        max: 4,
+        min: 1,
+      });
+    });
 
-        it( "Case #3", () => {
-            expect(findMaxMin([4, -3, 7, 1])).toStrictEqual( {
-                max: 7,
-                min: -3,
-            } );
-        })
+    it('Case #3', () => {
+      expect(findMaxMin([4, -3, 7, 1])).toStrictEqual({
+        max: 7,
+        min: -3,
+      });
+    });
 
-        it( "Case #4", () => {
-            expect(findMaxMin([-4, -3, -7, -1])).toStrictEqual( {
-                max: -1,
-                min: -7,
-            } );
-        })
-    })
+    it('Case #4', () => {
+      expect(findMaxMin([-4, -3, -7, -1])).toStrictEqual({
+        max: -1,
+        min: -7,
+      });
+    });
+  });
 
+  describe('Edge cases', () => {
+    it('Case #1. Empty array', () => {
+      expect(findMaxMin([])).toStrictEqual({
+        max: undefined,
+        min: undefined,
+      });
+    });
 
-    describe( "Edge cases", () => {
-        it("Case #1. Empty array", () => {
-            expect( findMaxMin( [] ) ).toStrictEqual( { 
-                max : undefined,
-                min : undefined,
-            } );
-        })
+    it('Case #2. Single element array', () => {
+      expect(findMaxMin([7])).toStrictEqual({
+        max: 7,
+        min: 7,
+      });
+    });
 
-        it("Case #2. Single element array", () => {
-            expect( findMaxMin( [7] ) ).toStrictEqual( { 
-                max : 7,
-                min : 7,
-            } );
-        })
-
-        it("Case #3. Same array elements", () => {
-            expect( findMaxMin( [5, 5, 5, 5] ) ).toStrictEqual( { 
-                max : 5,
-                min : 5,
-            } );
-        })
-    } )
-})
+    it('Case #3. Same array elements', () => {
+      expect(findMaxMin([5, 5, 5, 5])).toStrictEqual({
+        max: 5,
+        min: 5,
+      });
+    });
+  });
+});
