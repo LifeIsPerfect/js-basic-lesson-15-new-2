1af4288e14732cc37582e0ac817b9c8c4849994d
Alexander Skripko <lifeisperfect3@gmail.com>
2024-02-24 09:26:57 +0300

    Solve task 2 of the 5th homework;

--- src/arrays/arrays.js
+++ src/arrays/arrays.js
@@ -1,3 +1,15 @@
 const sum = ( arr ) => {
+    return arr.reduce( (accumulator, value) => accumulator + value, 0 );
+}
+
+//module.exports = sum;
+
+const doubleArrayValues = ( arr ) => {
+    return arr.map( el => 2*el );
+}
+
+const findMaxMin = () => {
     /* */
 }
+
+module.exports = { sum, doubleArrayValues, findMaxMin };
