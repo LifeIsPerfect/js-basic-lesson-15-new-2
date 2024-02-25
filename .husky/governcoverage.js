ca54c0977f908a9a62155afca6b853c81ba5b876
Alexander Skripko <lifeisperfect3@gmail.com>
2024-02-25 13:22:02 +0300

    Add ESLint configuration files;

--- .husky/governcoverage.js
+++ .husky/governcoverage.js
@@ -1,8 +1,8 @@
-let oldCoverage = require('../.lastcoverage.json');
-let newCoverage = require('../coverage/coverage-summary.json');
+let oldCoverage = require( '../.lastcoverage.json' );
+let newCoverage = require( '../.coverage/coverage-summary.json' );
 
 if( oldCoverage.total.lines.pct > newCoverage.total.lines.pct ||
     oldCoverage.total.lines.pct > newCoverage.total.lines.pct ||
     oldCoverage.total.lines.pct > newCoverage.total.lines.pct ) {
-    process.exit(1);
+    process.exit( 1 );
 }
